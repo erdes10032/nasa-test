7e1eb633b175cee6969e29b8a25680cbe12244b6
erdes10032 <erdes10032@gmail.com>
2026-02-27 17:52:08 +0300

    add admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from adminsortable2.admin import SortableAdminMixin
+from .models import SliderImage
+from django.utils.html import format_html
 
-# Register your models here.
+
+@admin.register(SliderImage)
+class SliderImageAdmin(SortableAdminMixin, admin.ModelAdmin):
+    list_display = ('title', 'image_preview', 'order')
+    list_editable = ('order',)
+    ordering = ('order',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="120" />',
+                obj.image.url
+            )
+        return '—'
+
+    image_preview.short_description = 'Превью'
