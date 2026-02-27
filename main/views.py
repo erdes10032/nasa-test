da0c8c50409d5a0dc935f10864f5388c9970fa70
erdes10032 <erdes10032@gmail.com>
2026-02-27 21:14:17 +0300

    add views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import SliderImage
 
-# Create your views here.
+
+def index(request):
+    slides = SliderImage.objects.all()
+    return render(request, 'index.html', {
+        'slides': slides
+    })
