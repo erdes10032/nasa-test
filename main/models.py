65cceaa421b7d9f08ff0bf73fed98131f001cd6b
erdes10032 <erdes10032@gmail.com>
2026-02-27 16:26:28 +0300

    add models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from filer.fields.image import FilerImageField
 
-# Create your models here.
+
+class SliderImage(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Название'
+    )
+    image = FilerImageField(
+        on_delete=models.CASCADE,
+        related_name='slider_images',
+        verbose_name='Изображение'
+    )
+    order = models.PositiveIntegerField(
+        default=0,
+        verbose_name='Порядок'
+    )
+
+    class Meta:
+        verbose_name = 'Слайд'
+        verbose_name_plural = 'Слайды'
+        ordering = ['order']
+
+    def __str__(self):
+        return self.title
